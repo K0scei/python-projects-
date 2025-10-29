0abdeb444d58d3157d10823a06078efaa36e9dee
Ersin Ege Tezgüler <ersinprivate1@gmail.com>
2025-10-29 18:37:21 +0300

    added data folder, fixed signing up.

--- project 1/server.py
+++ project 1/server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, send_from_directory, render_template, redirect
 import socket
 import os
 import threading
-
+import json
 
 """"
 HEADER = 64
@@ -59,39 +59,39 @@ def signup():
 
 	if request.method == 'POST':
 		username = request.form.get('username', '').strip()
+		password = request.form.get('password', '').strip()
 
 		if not username:
 			error = "The username can not be empty!"
 		elif len(username) < 4:
 			error = "The username should be more than 4 characters!"
-		elif len(username) > 12:
-			error = "The username should be less than 12 characters!"
 
 		if not error:
+			from werkzeug.security import generate_password_hash
+			hashed_password = generate_password_hash(password=password)
 			session['username'] = username
-			print(f"New user: {username}")
+			os.makedirs('./data', exist_ok=True)
+			#not ethical, but who cares at this point
+			#write data to the file /data/userdata.json
+			try:
+				with open('./data/userdata.json', "r", encoding='utf-8') as f:
+					data = json.load(f)
+			except (json.JSONDecodeError, FileNotFoundError):
+					data = {"users":{}}
+
+			if username not in data["users"]:
+				data["users"][username] = {"password": hashed_password,"uploadids": [], 'downloadids':[]}
+
+			with open('./data/userdata.json', "w", encoding='utf-8') as f:
+				json.dump(data, f, ensure_ascii=False, indent=4)
+				print(f"New user: {username} dumped to the file successfully!!")
+
 			return redirect(url_for('home'))
 	return render_template("signup.html", error=error, username=username)
 
-@app.route("/upload", methods=["GET", "POST"])
-def upload():
-	if request.method == "POST":
-		if "file" not in request.files:
-			flash("File Not Found!")
-			return redirect(url_for("upload"))
 
-		file = request.files["file"]
-		if file.filename == "":
-			flash("No File Selected!")
-			return redirect(url_for("upload"))
 
-		filename = file.filename
-		file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-		flash(f"{filename} uploaded successfully.")
-		return redirect(url_for("upload"))
-	return render_template("upload.html")
-
-# !!! ^use common whitespace (tab 4 spaces)
+# !!! ^use common whitespace (tab / 4 spaces)
 
 @app.route("/download/<filename>")
 def download(filename):
@@ -105,7 +105,7 @@ if __name__ == "__main__":
 @app.route("/home")
 def home():
 	username = session.get('username')
-	return render_template("home.html")
+	return render_template("home.html", username=username)
 
 @app.route("/logout")
 def logout():
