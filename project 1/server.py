f51cd0d72be9085a9bd9050cc2111eb5a565fe04
Ersin Ege Tezgüler <ersinprivate1@gmail.com>
2025-11-03 19:35:33 +0300

    dir fixed hopefully, revising sidebar

--- project 1/server.py
+++ project 1/server.py
@@ -40,7 +40,8 @@ while connected:
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
-UPLOAD_FOLDER = "uploads"
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok = True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
